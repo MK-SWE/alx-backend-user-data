2de384b5085e7b73ac657aa65a898101c1957313
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-31 18:22:35 +0000

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,7 +13,8 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Replaces sensitive information in a message """
     for field in fields:
-        msg = re.sub(f'{field}=.*?{separator}',f'{field}={redaction}{separator}', message)
+        msg = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return msg
 
 
