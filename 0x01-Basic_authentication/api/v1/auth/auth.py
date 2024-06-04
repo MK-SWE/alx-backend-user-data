6ce70c619cad1c15491c9179e33896cd1e873db3
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-04 23:09:55 +0000

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Auth module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    The template for all authentication system will be implemented
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns False - path and excluded_paths will be used later
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """  returns None - request will be the Flask request object
+        """
+        return request
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None - request will be the Flask request object
+        """
+        return request
