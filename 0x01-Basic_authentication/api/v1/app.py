d611cefe5c331a65344ec00c119967b7d7866191
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-05 00:44:17 +0000

    Implement request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -41,7 +41,7 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before_request_func():
+def before_request_func() -> any:
     if auth is None:
         return
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
