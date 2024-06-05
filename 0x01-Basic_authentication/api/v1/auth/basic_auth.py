1f16870f10d9299d07217df7bf0230c0002ca852
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-05 01:18:45 +0000

    Implement extract_base64_authorization_header function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,13 @@ class BasicAuth(Auth):
     """
     The template for basic authentication module
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str):
+            if authorization_header[0:6] != "Basic ":
+                return None
+            return authorization_header[6:]
+        else:
+            return None
