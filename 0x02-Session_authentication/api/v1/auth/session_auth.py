7b9b889272a07731b0952227245c49e62bdfd21b
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-06 14:55:52 +0000

    generate session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,19 @@
 Basic auth module for the API
 """
 from api.v1.auth.auth import Auth
-from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """
         The class that manage the session authentication mechanism
     """
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """ Generate Session
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        Session_ID = str(uuid.uuid4())
+        self.user_id_by_session_id[Session_ID] = user_id
+        return Session_ID
