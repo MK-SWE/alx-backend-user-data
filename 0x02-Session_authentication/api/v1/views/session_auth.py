71a3d412f5f8f81d09e612cede6d0aa42ddda2e8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-07 02:59:31 +0000

    Implement user logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,7 +34,9 @@ def login() -> Tuple[str, int]:
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def logout() -> Tuple[str, int]:
     """ Logout User
     """
