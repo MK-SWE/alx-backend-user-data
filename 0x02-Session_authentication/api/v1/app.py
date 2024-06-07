67f3fc03c838e63b2e624a8b31b6e53be69c6361
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-07 01:34:07 +0000

    Implement update @before_request middleware

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,14 +6,13 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
+AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -52,10 +51,15 @@ def request_middleware() -> any:
     """
     if auth is None:
         return
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and\
+       auth.session_cookie(request) is None:
         abort(401, description="Unauthorized")
     if auth.current_user(request) is None:
         abort(403, description="forbidden")
