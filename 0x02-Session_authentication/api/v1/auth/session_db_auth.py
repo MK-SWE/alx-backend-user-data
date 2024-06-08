e8b15189c93ece74f453905c9a29e4e4f003f1ff
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-08 23:55:15 +0300

    edit SessionDBAuth

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -4,6 +4,10 @@ Session db module for the API
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from datetime import (
+    datetime,
+    timedelta
+)
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -26,10 +30,18 @@ class SessionDBAuth(SessionExpAuth):
     def user_id_for_session_id(self, session_id=None):
         """ Overload user_id_for_session_id function
         """
-        user_id = UserSession.search({"session_id": session_id})
-        if user_id:
-            return user_id
-        return None
+        try:
+            sessionData = UserSession.search({'session_id': session_id})
+        except Exception:
+            return None
+        if len(sessionData) <= 0:
+            return None
+        currentTime = datetime.now()
+        time_span = timedelta(seconds=self.session_duration)
+        expire_time = sessionData[0].created_at + time_span
+        if expire_time < currentTime:
+            return None
+        return sessionData[0].user_id
 
     def destroy_session(self, request=None):
         """ Overload destroy_session function
@@ -37,10 +49,11 @@ class SessionDBAuth(SessionExpAuth):
         if request is None:
             return False
         session_id = self.session_cookie(request)
-        if not session_id:
+        try:
+            user_session = UserSession.search({'session_id': session_id})
+        except Exception:
             return False
-        user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            user_session[0].remove()
-            return True
-        return False
+        if len(user_session) <= 0:
+            return False
+        user_session[0].remove()
+        return True
