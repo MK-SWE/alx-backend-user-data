59d340ce266bfe218771e42d4b5fc3c2158e7cae
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-13 02:11:10 +0300

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,11 +4,13 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.route("/")
 def home() -> str:
     """Home page route
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
