a13fee83675832205808d8c82d556addce0696ed
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-13 02:27:57 +0300

    basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python3
 """Basic flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route("/")
+@app.route("/", methods=["GET"], strict_slashes=False)
 def home() -> str:
     """Home page route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """Register user endpoint
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email=email, password=password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
