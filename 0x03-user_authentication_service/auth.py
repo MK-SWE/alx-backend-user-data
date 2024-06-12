693f02cf5dc1772249eae9549fb9bd64bc131a07
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-13 02:04:00 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,7 +20,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """ Register the user
         """
         try:
