4e4666c103edb520046e6f29985599efcc6734ed
MohammedMagdy2023 <128869362+MohammedMagdy2023@users.noreply.github.com>
2024-06-13 13:19:02 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,7 +43,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
-  
+
+    def destroy_session(self, user_id: int) -> None:
+      """Destroy session
+      """
+      self._db.update_user(user_id= user_id, session_id=None)
+
     def get_user_from_session_id(self, session_id: str) -> Union[None | User]:
       """Find user by session ID
       """
