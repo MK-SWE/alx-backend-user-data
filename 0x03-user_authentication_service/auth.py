0c603c967f8f090d5456bb1dba0e7435583c5161
MohammedMagdy2023 <128869362+MohammedMagdy2023@users.noreply.github.com>
2024-06-13 13:23:06 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,20 +45,20 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: int) -> None:
-      """Destroy session
-      """
-      self._db.update_user(user_id= user_id, session_id=None)
+        """Destroy session
+        """
+        self._db.update_user(user_id= user_id, session_id=None)
 
     def get_user_from_session_id(self, session_id: str) -> Union[None | User]:
-      """Find user by session ID
-      """
-      if session_id is None:
-        return None
-      try:
-        user = self._db.find_user_by(session_id=session_id)
-      except NoResultFound:
-        return None
-      return user
+        """Find user by session ID
+        """
+        if session_id is None:
+          return None
+        try:
+          user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+          return None
+        return user
 
 
 def _hash_password(password: str) -> bytes:
