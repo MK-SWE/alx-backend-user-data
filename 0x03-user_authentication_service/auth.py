f704580a4505e94c0805e8c0739620d2ce78aa20
MohammedMagdy2023 <128869362+MohammedMagdy2023@users.noreply.github.com>
2024-06-13 13:06:45 +0300

    Test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,17 +42,17 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
-
+  
     def get_user_from_session_id(self, session_id: str) -> User | None:
-        """Find user by session ID
-        """
-        if not session_id:
-            return None
-        try:
-            user = self._db.find_user_by(session_id= session_id)
-            return user
-        except NoResultFound:
-            return None
+      """Find user by session ID
+      """
+      if session_id is None:
+        return None
+      try:
+        user = self._db.find_user_by(session_id=session_id)
+      except NoResultFound:
+        return None
+      return user
 
 
 def _hash_password(password: str) -> bytes:
